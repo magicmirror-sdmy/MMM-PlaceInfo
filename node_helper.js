aab6c84af9304def2a6a108975485fcd1ff5ada4
Dr.Sudhamoy Debbarma <65818001+sdmydbr9@users.noreply.github.com>
2024-06-01 00:10:07 +0530

    Update node_helper.js
    added cacheing

--- node_helper.js
+++ node_helper.js
@@ -7,6 +7,8 @@
 
 const NodeHelper = require("node_helper");
 const axios = require("axios");
+const fs = require("fs");
+const path = require("path");
 
 module.exports = NodeHelper.create({
   start: function () {
@@ -50,29 +52,47 @@ module.exports = NodeHelper.create({
           var weatherData = responses.map(function (response) {
             return response.data;
           });
-          console.log(this.name + ": Weather data received: ", weatherData);
+          console.log(self.name + ": Weather data received: ", weatherData);
           self.sendSocketNotification("WEATHER_DATA", { places: weatherData });
         })
       )
       .catch(function (error) {
-        console.error(this.name + ": Error getting weather data: ", error);
+        console.error(self.name + ": Error getting weather data: ", error);
       });
   },
 
   getCurrencies: function (payload) {
     console.log(this.name + ": Fetching currency data with API key: " + payload.currencyAPIKey);
     var self = this;
+    var cacheFile = path.resolve(__dirname, "currency_cache.json");
+
+    // Check if cached data exists and is still valid
+    if (fs.existsSync(cacheFile)) {
+      var cachedData = JSON.parse(fs.readFileSync(cacheFile, "utf8"));
+      var cacheTime = new Date(cachedData.timestamp * 1000);
+      var now = new Date();
+      var cacheAge = (now - cacheTime) / 1000; // age in seconds
+
+      // If cache is less than 4 hours old, use it
+      if (cacheAge < 4 * 60 * 60) {
+        console.log(this.name + ": Using cached currency data.");
+        self.sendSocketNotification("CURRENCY_DATA", cachedData);
+        return;
+      }
+    }
+
     var url = `${payload.currencyAPI}?access_key=${payload.currencyAPIKey}&base=${payload.currencyBase}`;
     console.log(this.name + ": Currency API URL: ", url);
 
     axios
       .get(url)
       .then(function (response) {
-        console.log(this.name + ": Currency data received: ", response.data);
+        console.log(self.name + ": Currency data received: ", response.data);
+        fs.writeFileSync(cacheFile, JSON.stringify(response.data));
         self.sendSocketNotification("CURRENCY_DATA", response.data);
       })
       .catch(function (error) {
-        console.error(this.name + ": Error getting currency data: ", error);
+        console.error(self.name + ": Error getting currency data: ", error);
       });
   }
 });
