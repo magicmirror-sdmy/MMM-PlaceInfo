29a9d48303b43bf937dca5df248a9abfbaccef42
Dr.Sudhamoy Debbarma <65818001+sdmydbr9@users.noreply.github.com>
2024-05-31 22:05:01 +0530

    Update MMM-PlaceInfo.js

--- MMM-PlaceInfo.js
+++ MMM-PlaceInfo.js
@@ -12,7 +12,7 @@ Module.register("MMM-PlaceInfo", {
     symbols: null,
     units: config.units,
     animationSpeed: 1000,
-    updateInterval: 1000 * 3600, //update every hour
+    updateInterval: 1000 * 3600, // update every hour
     timeFormat: "HH:mm A",
     lang: config.language,
     showCustomHeader: false,
@@ -20,15 +20,12 @@ Module.register("MMM-PlaceInfo", {
     showFlag: true,
     showText: true,
 
-    // would make this more structured, but the config is not merged from the
-    // the user config file, but replaces things, so the whole structure would be
-    // blitzed if the user wanted to change one property.
     weatherUnits: config.units,
     weatherAPI: "https://api.openweathermap.org/data/",
     weatherAPIEndpoint: "group",
     weatherAPIVersion: "2.5",
     weatherAPIKey: "",
-    weatherInterval: 10 * 60 * 1000, // every 10 minutes. Allowed 60calls/min
+    weatherInterval: 10 * 60 * 1000, // every 10 minutes
     weatherLoadDelay: 0,
     weatherRetryDelay: 2500,
     weatherPrecision: 0,
@@ -55,35 +52,24 @@ Module.register("MMM-PlaceInfo", {
     },
 
     currencyAPI: "http://data.fixer.io/api/latest",
-    currencyBase: "EUR", // cannot change, unless you're using a paid-up plan.
+    currencyBase: "EUR",
     currencyRelativeTo: "EUR",
     currencyReversed: false,
-    currencyPrecision: 3, // how many decimal places
-    currencyInterval: 4 * 60 * 60 * 1000, // 4hr. don't want frequent since free limit is 2k/mon
+    currencyPrecision: 3,
+    currencyInterval: 4 * 60 * 60 * 1000, // 4hr
     currencyLoadDelay: 0,
-    currencyRetryDelay: 60 * 60 * 1000, // retry hourly if something is broken
-
-    // Example place
-    places: [
-      {
-        title: "New York",
-        timezone: "America/New_York",
-        flag: "us",
-        currency: "USD",
-        weatherID: 5128581
-      }
-    ]
+    currencyRetryDelay: 60 * 60 * 1000 // retry hourly
   },
 
   state: {
     weather: {
       timer: undefined,
-      values: {}, // results of lookup
+      values: {},
       loaded: false
     },
     currency: {
       timer: undefined,
-      values: {}, // results of currency lookup
+      values: {},
       loaded: false
     }
   },
@@ -119,34 +105,28 @@ Module.register("MMM-PlaceInfo", {
   },
 
   start: function () {
-    Log.info(this.name + ": Starting module");
     this.loadCSS();
 
-    this.state.cstatus = "Loading"; // currency summary status
-    this.state.wstatus = "Loading"; // weather summary status
-    this.state.weather.fn = this.updateWeather;
+    this.state.cstatus = "Loading"; 
+    this.state.wstatus = "Loading";
+    this.state.weather.fn = this.updateWeather.bind(this);
     this.state.weather.interval = this.config.weatherInterval;
-    this.state.currency.fn = this.updateCurrencies;
+    this.state.currency.fn = this.updateCurrencies.bind(this);
     this.state.currency.interval = this.config.currencyInterval;
 
     if (!this.config.currencyAPIKey) {
-      Log.info(
-        this.name + ": No API key for currencies: disabling currency lookup"
-      );
       this.state.csummary = "disabled (no API key)";
     } else {
+      this.sendSocketNotification("LOG", this.name + ": Scheduling currency update with API key: " + this.config.currencyAPIKey);
       this.scheduleUpdate(this.state.currency, this.config.currencyLoadDelay);
     }
     if (!this.config.weatherAPIKey) {
-      Log.info(
-        this.name + ": No API key for weather: disabling weather lookup"
-      );
       this.state.wsummary = "disabled (no API key)";
     } else {
+      this.sendSocketNotification("LOG", this.name + ": Scheduling weather update with API key: " + this.config.weatherAPIKey);
       this.scheduleUpdate(this.state.weather, this.config.weatherLoadDelay);
     }
 
-    // guarantee that we always have a valid base
     if (this.config.currencyBase == "") {
       this.config.currencyBase = "EUR";
     }
@@ -160,10 +140,9 @@ Module.register("MMM-PlaceInfo", {
   getDom: function () {
     var wrapper = document.createElement("div");
 
-    // display a custom header, just in case
     if (this.config.showCustomHeader) {
       var customHeader = document.createElement("div");
-      currencyInfo = "";
+      var currencyInfo = "";
       if (!this.config.currencyAPIKey) {
         currencyInfo += "Currency data disabled (no API key)\n";
       } else {
@@ -184,7 +163,6 @@ Module.register("MMM-PlaceInfo", {
       wrapper.appendChild(customHeader);
     }
 
-    // get ready to process data and print it to the screen
     var outputWrapper = document.createElement("div");
 
     var dataLimit = 3;
@@ -204,7 +182,6 @@ Module.register("MMM-PlaceInfo", {
     for (var placeIdx in this.config.places) {
       var place = this.config.places[placeIdx];
       if (this.config.layoutStyle == "table") {
-        // we want more columns to save some space on screen, so we iterate and only create a new row with every second dataset
         if (placeIdx % dataLimit == 0) {
           var row = document.createElement("tr");
         }
@@ -212,7 +189,6 @@ Module.register("MMM-PlaceInfo", {
       }
       var placeContainer = document.createElement("span");
 
-      // determine if user wants to see the flag
       if (place.flag != "") {
         var flagWrapper = document.createElement("div");
         flagWrapper.className = "flag";
@@ -241,7 +217,6 @@ Module.register("MMM-PlaceInfo", {
       timeWrapper.className = "time small";
       placeContainer.appendChild(timeWrapper);
 
-      // determine if user wants to see the abbreviated currency text (EUR, USD, ..)
       if (place.currency != "") {
         var currencySpan = document.createElement("div");
         currencySpan.className = "currency small";
@@ -267,252 +242,79 @@ Module.register("MMM-PlaceInfo", {
         } else {
           var weather = this.state.weather.values[placeIdx];
           var weatherIcon = document.createElement("span");
-          var icon = this.config.weatherIcons[weather.weather[0].icon];
-          weatherIcon.className = "wi weathericon " + icon;
-          weatherSpan.appendChild(weatherIcon);
-
-          var degreeLabel = "";
-          if (this.config.degreeLabel) {
-            switch (this.config.units) {
-              case "metric":
-                degreeLabel = "C";
-                break;
-              case "imperial":
-                degreeLabel = "F";
-                break;
-              case "default":
-                degreeLabel = "K";
-                break;
-            }
-          }
+          var weatherClass =
+            "wi weathericon " + this.config.weatherIcons[weather.weather[0].icon];
+          weatherIcon.className = weatherClass;
+          weatherIcon.innerHTML = "&nbsp;";
           var weatherTemp = document.createElement("span");
           weatherTemp.innerHTML =
             weather.main.temp.toFixed(this.config.weatherPrecision) +
             "&deg;" +
-            degreeLabel;
-
+            (this.config.weatherUnits == "imperial" ? "F" : "C");
+          weatherTemp.className = "bright";
+          weatherSpan.appendChild(weatherIcon);
           weatherSpan.appendChild(weatherTemp);
         }
         placeContainer.appendChild(weatherSpan);
       }
 
-      // if the user wants a table, we add the dataset to a cell. If this is the last dataset for a row or the final dataset we add the row to the table
       if (this.config.layoutStyle == "table") {
         cell.appendChild(placeContainer);
         row.appendChild(cell);
-        if (
-          placeIdx % dataLimit != 0 ||
-          placeIdx == this.config.places.length - 1
-        ) {
-          table.appendChild(row);
-        }
+        table.appendChild(row);
       } else {
         outputWrapper.appendChild(placeContainer);
       }
     }
-
     if (this.config.layoutStyle == "table") {
       outputWrapper.appendChild(table);
-      wrapper.appendChild(outputWrapper);
-    } else if (this.config.layoutStyle == "ticker") {
-      var marqueeTicker = document.createElement("marquee");
-      marqueeTicker.innerHTML = outputWrapper.innerHTML;
-      marqueeTicker.className = "small thin light";
-      marqueeTicker.width = document.getElementsByClassName(
-        "MMM-PlaceInfo"
-      )[0].clientWidth;
-      marqueeTicker.scrollDelay = 100;
-      wrapper.appendChild(marqueeTicker);
     }
+    wrapper.appendChild(outputWrapper);
 
     return wrapper;
   },
 
+  scheduleUpdate: function (what, delay) {
+    if (what.timer) {
+      clearTimeout(what.timer);
+    }
+    what.timer = setTimeout(function () {
+      what.fn();
+    }, delay);
+  },
+
   updateWeather: function () {
-    var params = this.getWeatherParams();
-    if (params == "") {
-      console.log(this.name + ": no weathers to request");
-      return;
-    }
-    var url =
-      this.config.weatherAPI +
-      this.config.weatherAPIVersion +
-      "/" +
-      this.config.weatherAPIEndpoint +
-      params;
-    var self = this;
-    var retry = true;
-    console.log(this.name + ": weather request(" + url + ")");
-    var weatherRequest = new XMLHttpRequest();
-    weatherRequest.open("GET", url, true);
-    weatherRequest.onreadystatechange = function () {
-      if (this.readyState === 4) {
-        if (this.status === 200) {
-          self.processWeather(JSON.parse(this.response));
-        } else if (this.status === 401) {
-          self.updateDom(self.config.animationSpeed);
-          Log.error(self.name + ": Incorrect API Key for weather.");
-          self.state.wstatus = "failed (bad API key)";
-          retry = false;
-        } else {
-          Log.error(self.name + ": Could not load weather: " + this.status);
-          self.state.wstatus = "failed (HTTP " + this.status + ")";
-        }
-
-        if (retry) {
-          self.scheduleUpdate(
-            self.state.weather,
-            self.state.weather.loaded ? -1 : this.config.weatherRetryDelay
-          );
-        }
-      }
-    };
-    weatherRequest.send();
-  },
-
-  getWeatherParams: function () {
-    var placeIDs = {};
-    for (var placeIdx in this.config.places) {
-      var place = this.config.places[placeIdx];
-      if (place.weatherID) {
-        placeIDs[place.weatherID] = 1;
-      }
-    }
-    if (Object.keys(placeIDs).length == 0) {
-      return "";
-    }
-    var params = "?APPID=" + this.config.weatherAPIKey;
-    params += "&id=" + Object.keys(placeIDs).join();
-    params += "&units=" + this.config.weatherUnits;
-    return params;
-  },
-
-  processWeather: function (data) {
-    if (!data || !data.cnt) {
-      Log.error(
-        this.name + ": failed to process weather data: " + JSON.stringify(data)
-      );
-      this.state.wstatus = "failed (" + JSON.stringify(data) + ")";
-      return;
-    }
-    this.state.weather.values = [];
-    for (var cityIdx in data.list) {
-      for (var placeIdx in this.config.places) {
-        if (
-          this.config.places[placeIdx].hasOwnProperty("weatherID") &&
-          this.config.places[placeIdx].weatherID == data.list[cityIdx].id
-        ) {
-          this.state.weather.values[placeIdx] = data.list[cityIdx];
-        }
-      }
-    }
-    this.state.weather.loaded = true;
-    this.updateDom(this.config.animationSpeed);
+    this.sendSocketNotification("GET_WEATHER", this.config);
   },
 
   updateCurrencies: function () {
-    var url = this.config.currencyAPI;
-    var delay = this.config.currencyRetryDelay;
-    var params = this.getCurrencyParams();
-    if (params == "") {
-      Log.info(this.name + ": no currencies to request");
-      this.state.cstatus = "incomplete config";
-      return;
-    }
-    url += params;
-    var self = this;
-    var retry = true;
-
-    console.log(this.name + ": currency request(" + url + ")");
-    var Request = new XMLHttpRequest();
-    Request.open("GET", url, true);
-    Request.onreadystatechange = function () {
-      if (this.readyState === 4) {
-        if (this.status === 200) {
-          self.processCurrencies(JSON.parse(this.response));
-        } else {
-          Log.error(self.name + ": Could not load data.");
-          self.state.cstatus = "failed (" + this.status + ")"
-        }
-        self.scheduleUpdate(
-          self.state.currency,
-          self.state.currency.loaded ? -1 : delay
-        );
-      }
-    };
-    Request.send();
+    this.sendSocketNotification("GET_CURRENCIES", this.config);
   },
 
-  getCurrencyParams: function () {
-    var currencies = {};
-    for (var placeIdx in this.config.places) {
-      var place = this.config.places[placeIdx];
-      if (place.currency) {
-        currencies[place.currency] = 1;
-      }
+  socketNotificationReceived: function (notification, payload) {
+    if (notification === "WEATHER_DATA") {
+      this.processWeather(payload);
     }
-    if (Object.keys(currencies).length == 0) {
-      return "";
+    if (notification === "CURRENCY_DATA") {
+      this.processCurrencies(payload);
     }
-    if (this.config.currencyBase != this.config.currencyRelativeTo) {
-      currencies[this.config.currencyRelativeTo] = 1;
+    if (notification === "LOG") {
+      console.log(payload);
     }
-    params = "?access_key=" + this.config.currencyAPIKey;
-    params += "&base=" + this.config.currencyBase;
-    params += "&symbols=" + Object.keys(currencies).join();
-    return params;
+  },
+
+  processWeather: function (data) {
+    this.state.weather.values = data.places;
+    this.state.weather.loaded = true;
+    this.state.wstatus = "Loaded";
+    this.updateDom();
   },
 
   processCurrencies: function (data) {
-    if (!data.rates) {
-      // Did not receive usable new data.
-      // Maybe this needs a better check?
-      Log.error(
-        this.name +
-          ": failed to process currency data: " + data.error.info
-      );
-      this.state.cstatus = "failed (" + data.error.info + ")";
-      return;
-    }
-
-    this.rateUpdate = data.date;
     this.state.currency.values = data.rates;
-    // if we got data in a different base, we need to convert
-    if (this.config.currencyBase != this.config.currencyRelativeTo) {
-      var conversion = this.state.currency.values[
-        this.config.currencyRelativeTo
-      ];
-      if (!conversion) {
-        conversion = 1; // prevent stupidness
-      }
-      for (var cur in this.state.currency.values) {
-        this.state.currency.values[cur] /= conversion;
-      }
-    }
-    if (this.config.currencyReversed) {
-        for (var cur in this.state.currency.values) {
-            this.state.currency.values[cur] = 1 / this.state.currency.values[cur];
-        }
-    }
-    for (var cur in this.state.currency.values) {
-      this.state.currency.values[cur] = this.state.currency.values[cur].toFixed(
-        this.config.currencyPrecision
-      );
-    }
     this.state.currency.loaded = true;
-    this.updateDom(this.config.animationSpeed);
-  },
-
-  scheduleUpdate: function (cnf, delay) {
-    var self = this;
-    var nextLoad = cnf.interval;
-    if (typeof delay !== "undefined" && delay >= 0) {
-      nextLoad = delay;
-    }
-
-    clearTimeout(cnf.timer);
-    cnf.timer = setTimeout(function () {
-      cnf.fn.call(self);
-    }, nextLoad);
+    this.state.cstatus = "Loaded";
+    this.rateUpdate = moment.unix(data.timestamp).fromNow();
+    this.updateDom();
   }
 });
